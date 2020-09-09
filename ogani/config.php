eb1f57ae60c643f2b7c82b0a606416ececd38b30
jaslin_tan <jaslin_tan@mail.chungyo.net>
2020-09-09 10:43:00 +0800

    delete no use html

--- ogani/config.php
+++ ogani/config.php
@@ -4,9 +4,5 @@
 	$dbuser = 'root';
 	$dbpass = 'root';
 	$dbname = 'shoppingCart';
-	// $dbhost = '127.0.0.1';
-	// $dbuser = 'root';
-	// $dbpass = 'Jaslin;0323mac';
-	// $dbname = 'shoppingCart';
 
 ?>
